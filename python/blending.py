5648d1f1bf0545412eda99c9da2cd9bb02c89eb3
Ferenc Huszar <fhuszar@eduroam1-075.eng.cam.ac.uk>
2012-05-08 14:40:09 +0100

    Ferencs part added to Descroption.txt

--- python/blending.py
+++ python/blending.py
@@ -3,11 +3,11 @@ import numpy as np
 
 p = 0.25
 
-raw_bench = csv.reader(open('sample_code/submission_hour_chunk_means.csv'))
+raw_bench = csv.reader(open('../sample_code/submission_hour_chunk_means.csv'))
 
-raw_ours = csv.reader(open('submissions/2012_04_29-10_10.csv'))
+raw_ours = csv.reader(open('../submissions/2012_04_29-10_10.csv'))
 
-blended = csv.writer(open('submissions/blended25.csv','w'))
+blended = csv.writer(open('../submissions/blended25.csv','w'))
 
 blended.writerow(raw_ours.next())
 
